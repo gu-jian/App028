ca7ae7deaf74040c4f6a03ec4a43ab55b41dbdbd
gujian-china <gujian_china@163.com>
2015-11-27 17:34:49 +0800

    commit app028 by gujian

--- app/src/main/java/com/example/gujian/app028/MainActivity.java
+++ app/src/main/java/com/example/gujian/app028/MainActivity.java
@@ -119,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
 
                 //ddddddcccc
+                return;
             }
         });
 
